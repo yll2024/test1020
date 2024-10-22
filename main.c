f8f959efe517e221248d3a4e5f42597629bc68cc
yll2024 <1063046544@qq.com>
2024-10-22 22:21:24 +0800

    添加main函数代码

---
+++ main.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+     #include<stdlib.h>
+     #include<string.h>
+     #include<stdio.h>
+     #include<unistd.h>
+     #include<pthread.h>
+    #include<curl/curl.h>
+
+    int main(int argc, char *argv[])
+    {
+
+        printf("nihao itcast!\n");
+        printf("nihuai ithm\n");
+        return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+A
+A
+A
+A
+A
+A
+    }
